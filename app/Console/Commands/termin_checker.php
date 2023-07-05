b5da114de118451808ee97ac67fb64628d29a89a
NIkolai Demin <demin.nikolai@inbox.ru>
2023-07-05 14:05:34 +0300

    added needle2

--- app/Console/Commands/termin_checker.php
+++ app/Console/Commands/termin_checker.php
@@ -39,8 +39,9 @@ class termin_checker extends Command
         $url = TerminUrlModel::first()->url;
         $html = file_get_contents($url, false, $context);
         $needle = 'Leider sind aktuell keine Termine für ihre Auswahl verfügbar';
+        $needle2 = 'Wartung';
 
-        if (strripos($html, $needle) === false) {
+        if (strripos($html, $needle) === false && strripos($html, $needle2) === false) {
             Mail::send('emails.termin-notify', ['termin_url' => $url], function($message)
             {
                 $message->to(config('termin.notify_email'), config('termin.notify_name'))
